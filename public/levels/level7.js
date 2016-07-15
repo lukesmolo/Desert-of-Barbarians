a69fb1f6ec334f102e5cfcaff8f850dc7c5a1fe9
Alessandro Cocilova <alessandro.cocilov@gmail.com>
2016-07-15 12:55:44 +0200

    removed solution code from  lev 7 8 9

--- public/levels/level7.js
+++ public/levels/level7.js
@@ -1,10 +1,3 @@
 function playerShoot2(x,y) {
-	if( checkHeight(y) ) {
-		var source = whichAntiMissileBattery( x );
-		if( source === -1 ){
-			return;
-		}
-		playerMissiles.push( new PlayerMissile( source, x, y ) );
-		playerMissiles.push( new PlayerMissile( source, x + 35, y ) );
-	}
+
 }
--- public/levels/level9.js
+++ public/levels/level9.js
@@ -1,13 +1,3 @@
 function whichAntiMissileBatteryObf(x) {
-	if( x <= CANVAS_WIDTH / 3 ){
-		return firedToOuterThird( 0, 1, 2 );
-	} else if( x <= (2 * CANVAS_WIDTH / 3) ) {
-		if ( antiMissileBatteries[1].hasMissile() ) {
-			return 1;
-		} else {
-			return ( x <= CANVAS_WIDTH / 2 ) ? firedtoMiddleThird( 0, 2 )	: firedtoMiddleThird( 2, 0 );
-		}
-	} else {
-		return firedToOuterThird( 2, 1, 0 );
-	}
+
 }
